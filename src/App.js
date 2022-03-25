b97af67f24eeb054613a33e0b3b265fe4e97266d
Vishal Srivastava <vishalsrivastava69346@gmail.com>
2022-03-25 23:47:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg";
+import Posts from "./components/Posts";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Logo style={{ height: 200 }} />
+       <h1>Hello World Project</h1>
       </header>
+      <Posts/>
     </div>
   );
 }
